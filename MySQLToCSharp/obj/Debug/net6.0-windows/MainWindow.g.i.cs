7f2f9afe4696c511630de7229d17ba3403f0f16f
ErbosynNurbol <ErbosynNurbol@gmail.com>
2021-12-20 14:29:47 +0600

    Added screenshot

--- MySQLToCSharp/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ MySQLToCSharp/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -54,7 +54,7 @@ namespace MySQLToCSharp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MySQLToCSharp;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MySQLToCSharp;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
